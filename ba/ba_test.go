37ffb4a0dbfab300900eb3bbd04a0cff2cc6f62c
satit13 <satit@nopadol.com>
2018-09-19 09:50:09 +0700

    update code add ba_test

--- ba/ba_test.go
+++ ba/ba_test.go
@@ -4,33 +4,29 @@ import (
 	"testing"
 	"github.com/payboxth/vending/host/hw/cctalk"
 	"fmt"
-	"log"
+	//"log"
 	"github.com/payboxth/vending/host/model"
+	"github.com/magiconair/properties/assert"
 )
 
-func Test_CcTalk_Connect(t *testing.T) {
+func Test_CcTalk_Open(t *testing.T) {
 	CCT := &cctalk.Driver{}
 	// try to connect CCT Port
 	err := CCT.OpenConnection("/dev/ttyCCTALK", 9600)
-	if err != nil {
-		log.Println("Error OpenConnection ccTalk state : ", err)
-		t.Errorf("expect cctalk connnect true but actual is %s", err.Error())
-		return
-	} else {
-		fmt.Println("CCT Connected...")
-		CCT.Close()
-		return
-	}
-
+	defer CCT.Close()
+	assert.Equal(t, err.Error(), "open /dev/ttyCCTALK: no such file or directory", "errror @!!!")
 }
 
-func Test_BA_Connect(t *testing.T) {
+func Test_BA_open(t *testing.T) {
 	CCT := &cctalk.Driver{}
 	// try to connect CCT Port
 	err := CCT.OpenConnection("/dev/ttyCCTALK", 9600)
+	defer CCT.Close()
 	if err != nil {
-		t.Errorf("expect cctalk connect but --> %v", err.Error())
+		//t.Errorf("expect cctalk connect but --> %v", err.Error())
+		assert.Matches(t, err.Error(), "open /dev/ttyCCTALK: no such file or directory")
 	}
+
 	BA := model.BillAcceptor{
 		DeviceID:       0x28,
 		Status:         "idle",
@@ -46,41 +42,9 @@ func Test_BA_Connect(t *testing.T) {
 		if err != nil {
 			t.Errorf("expect BA connnected true but actual is ", err.Error())
 			fmt.Println("BA Open Error : ", err.Error())
-			BA.Close()
-			return
+			assert.Matches(t, err.Error(), "open /dev/ttyCCTALK: no such file or directory")
 		}
+		defer BA.Close()
 	}
-}
-
-func Test_BA_Open(t *testing.T) {
-	CCT := &cctalk.Driver{}
-	// try to connect CCT Port
-	err := CCT.OpenConnection("/dev/ttyCCTALK", 9600)
-	if err != nil {
-		t.Errorf("expect cctalk connect but --> %v", err.Error())
-	}
-	CCT.StartPolling()
-	BA := model.BillAcceptor{
-		DeviceID:       0x28,
-		Status:         "idle",
-		Send:           make(chan *model.Message, 10),
-		//TakeRejectNote: make(chan bool),
-		EscrowValue:    make(chan float64, 1),
-		StackedValue:   make(chan float64, 1),
-	}
-
-	if CCT.Online && !CCT.Busy {
-		fmt.Println(" try BA.Open(*CCT)...")
-		err := BA.Open(*CCT)
-		if err != nil {
-			t.Errorf("expect BA connnected true but actual is ", err.Error())
-			fmt.Println("BA Open Error : ", err.Error())
-			return
-		}
-	}
-
-	BA.SetEnable(true)
-	BA.Close()
-	return
 
 }
