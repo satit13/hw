7dcc665c30f433ded99b24868defb62f44d5ed3d
satit13 <satit@nopadol.com>
2018-09-20 11:16:57 +0700

    update code

--- nd/nd_test.go
+++ nd/nd_test.go
@@ -2,8 +2,29 @@ package nd_test
 
 import (
 	"testing"
+	"github.com/payboxth/vending/host/model"
+	"github.com/payboxth/vending/host/hw"
+	"log"
+	"github.com/magiconair/properties/assert"
 )
 
-func Test_nd_online(t *testing.T) {
+func ND_Init() error {
+	ND := &model.NoteDispenser{
+		DeviceId:        0,
+		ChangeNoteValue: 20,
+	}
+	err := ND.Open("/dev/ttyS0")
+	if err != nil {
+		if err.Error() == hw.ND_WIRING_WRONG_CABLE_WITH_OTHER_DEVICE {
+			log.Println("สงสัยเสียบสายอนุกรมสลับกับเครื่องสแกนบาร์โค้ด")
+		}
+		log.Println("Unknown Error:", err)
+		return err
+	}
+	return nil
+}
 
+func Test_nd_NoDevice_Should_Fail(t *testing.T) {
+	err := ND_Init()
+	assert.Equal(t, err.Error(), "open /dev/ttyS0: permission denied", "")
 }
